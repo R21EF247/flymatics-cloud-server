0b16872140916fcf9a140e54540971ba20a5a83f
Pratik K <146062312+R21EF247@users.noreply.github.com>
2023-11-11 05:50:54 +0000

    refactor: remove commented code

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// io.on('connection', (socket) => {
-//     console.log('Client connected:', socket.id);
-
-//     // Listen for drone connection
-//     socket.on('drone-connected', () => {
-//         console.log('Drone connected');
-//         socket.join('drone-room');
-//     });
-
-//     socket.on('send-number', (number) => {
-//         console.log('Received number:', number);
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('Client disconnected:', socket.id);
-//     });
-// });
-
-// server.listen(3000, () => {
-//     console.log('Cloud server listening on port 3000');
-// });
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
