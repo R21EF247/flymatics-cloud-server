d13523761ffec11e098d798e5bc22b874e336cea
Pratik K <146062312+R21EF247@users.noreply.github.com>
2023-11-16 22:32:18 +0000

    feat: remove pilot socket

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ io.on('connection', (socket) => {
         io.in('control-station-room').emit('video-stream', base64Data);
     });
 
+    // Listen for control station connection
+    socket.on('control-station-connected', () => {
+        console.log('Control station connected');
+        socket.join('control-station-room');
+    });
+
+    socket.on('Data Received from Nano', (nanoData) => {
+        console.log('Nano port data received:', nanoData);
+        socket.to('control-station-room').emit('forwarded-nano-data', nanoData);
+    });
+    
+    // Forward Pico port data to control station
+    socket.on('Data Received from Pico', (picoData) => {
+        console.log('Pico port data received:', picoData);
+        socket.to('control-station-room').emit('forwarded-pico-data', picoData);
+    });
+    
     socket.on('disconnect', () => {
         console.log('Client disconnected:', socket.id);
     });
